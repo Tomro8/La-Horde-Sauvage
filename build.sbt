16412d8291a2c5e2116eec8bce2e071f8e8f8451
Tomro8 <38689975+Tomro8@users.noreply.github.com>
2019-03-29 16:49:29 +0100

    First file in SRC

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := "Projet"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+libraryDependencies += "org.apache.kafka" %% "kafka" % "2.1.0"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime
---
+++ src/main/scala/Producer.scala
@@ -0,0 +1,38 @@
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
+import org.apache.kafka.common.serialization.StringSerializer
+
+import scala.io.Source
+
+object producer1 extends App{
+
+
+  val props: Properties = new Properties()
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+
+  val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+
+  val key = "key1"
+  val value = "value1"
+  val record = new ProducerRecord[String, String]("test1", key, value)
+
+  producer.send(record, (recordMetadata: RecordMetadata, exception: Exception) => {
+    if (exception != null) {
+      exception.printStackTrace()
+    } else {
+      println(s"Metadata about the sent record: $recordMetadata")
+    }
+  })
+
+  producer.close()
+
+  /*
+  Source.fromFile("/shared_data/Functionnal_Data_Programming/TP/TP2/TP2 Kafka.pdf")
+    .getLines()
+    .drop(1)
+    .foreach()
+    */
+}
